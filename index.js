6e65ce7285f977486fa658657803b71b4407f3f4
makis <avnikolaou@hotmail.com>
2019-04-28 23:22:37 +0300

    Initial commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res) => {
-    res.send({ hi: 'Broooo'});
+    res.send({ hi: 'Brother'});
 });
 
 const PORT = process.env.PORT || 5000;
