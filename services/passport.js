af07019b86fb0bf867e9c7d9865139147bb03e88
AvraamNikolaou <avnikolaou@hotmail.com>
2019-05-14 13:30:31 +0300

    Added: Some FrontEnd work, call to backend with actions to authReducer from redux

--- services/passport.js
+++ services/passport.js
@@ -22,20 +22,16 @@ passport.use(
             clientSecret: keys.googleClientSecret,
             callbackURL: '/auth/google/callback',
             proxy: true
-        }, (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id })
-                .then((existingUser) => {
-                    if(existingUser) {
-                       // We have a record of that user
-                        done(null, existingUser);
-                    } else {
-                        // we dont have a record
-                        new User ({ googleId: profile.id })
-                            .save()
-                            .then(user => done(null, user));
-                    }
-                }
-            );
+        }, async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleId: profile.id });
+            if(existingUser) {
+               // We have a record of that user
+                done(null, existingUser);
+            } else {
+                // we don't have a record
+                const user = await new User ({ googleId: profile.id }).save();
+                done(null, user);
+            }
         }
     )
 );
